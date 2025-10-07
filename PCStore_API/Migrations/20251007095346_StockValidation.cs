24f0632801fbcd4885fe533f5df2408b50c6bfc6
Ghrimy <dennis-beelen@hotmail.com>
2025-10-07 11:57:09 +0200

    Add migration for stock validation and shopping cart total price

---
+++ PCStore_API/Migrations/20251007095346_StockValidation.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PCStore_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class StockValidation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "TotalPrice",
+                table: "ShoppingCart",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "RowVersion",
+                table: "Products",
+                type: "varbinary(max)",
+                nullable: false,
+                defaultValue: new byte[0]);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TotalPrice",
+                table: "ShoppingCart");
+
+            migrationBuilder.DropColumn(
+                name: "RowVersion",
+                table: "Products");
+        }
+    }
+}
